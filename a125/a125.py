dee464a959936f8a629509f9463e176eeb110012
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-11-19 10:54:52 -0500

    added the spaces

--- a125/a125.py
+++ a125/a125.py
@@ -8,6 +8,9 @@ wn.setup(width=.5, height=.5)
 wn.bgpic("board.png")# Make the background of the scene
 print("hello")
 
+xcord = 50
+ycord = -50
+
 spaceTL = trtl.Turtle()
 spaceTM = trtl.Turtle()
 spaceTR = trtl.Turtle()
@@ -17,6 +20,18 @@ spaceMR = trtl.Turtle()
 spaceBL = trtl.Turtle()
 spaceBM = trtl.Turtle()
 spaceBR = trtl.Turtle()
+
+spaceList = [spaceTL, spaceTM, spaceTR, spaceML, spaceMM, spaceMR, spaceBL, spaceBM, spaceBR]
+
+for spaces in spaceList:
+    spaces.goto(xcord, ycord)
+    spaces.square(20)
+    xcord += 50
+    i = 0
+    if (i == 3):
+        ycord -= 50
+        i = 0
+    i += 1
 
 
 wn.listen()
