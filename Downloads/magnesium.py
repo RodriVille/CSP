864b9e62582efe246a6ccdf805fd5a9a5da35601
RodriVille <rodrigo.villegas.venegas@gmail.com>
2021-01-06 10:32:25 -0500

    DONE

--- Downloads/magnesium.py
+++ Downloads/magnesium.py
@@ -4,7 +4,7 @@ def monitor():
     val1 = 1250
     val2 = 1350
 
-    mag_levels = list(range(val1, val2, 10))
+    mag_levels = list(range(val1, val2))
 
     current = get_magnesium_level()
     mesg = "Magnesium level OK"
@@ -12,7 +12,7 @@ def monitor():
     num_levels = len(mag_levels)
     if (current < mag_levels[0]):
       mesg = "Magnesium level too low!"
-    elif (current > mag_levels[num_levels]):
+    elif (current > mag_levels[num_levels-1]):
       mesg = "Magnesium level too high!"
     
   except:
