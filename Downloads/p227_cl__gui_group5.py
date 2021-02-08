0b17ef31ca1a626082d7bc14e9d511f89fedd884
RodriVille <rodrigo.villegas.venegas@gmail.com>
2021-02-08 12:43:42 -0500

    ok

--- Downloads/p227_cl__gui_group5.py
+++ Downloads/p227_cl__gui_group5.py
@@ -7,7 +7,7 @@ from tkinter.filedialog import asksaveasfilename
 
 def do_command(command):
     global command_textbox, url_entry
-    # If url_entry is blank, use localhost IP address 
+    # If url_entry is blank, use localhost IP address
     url_val = url_entry.get()
     if (len(url_val) == 0):
         # url_val = "127.0.0.1"
@@ -23,6 +23,17 @@ def do_command(command):
     command_textbox.insert(tk.END, cmd_results)
     command_textbox.insert(tk.END, cmd_errors)
 
+# Save function.
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  
+  file.write(text_to_save)
+  file.close()
+
 
 root = tk.Tk()
 frame = tk.Frame(root)
@@ -30,8 +41,6 @@ root.geometry("500x500")
 frame.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=do_command)
-ping_btn.pack()
 
 frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
 frame_URL.pack()
@@ -57,14 +66,17 @@ command_textbox = tksc.ScrolledText(frame, height=10, width=100)
 command_textbox.pack()
 
 # Makes the command button pass it's name to a function using lambda
-ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command("ping"))
+ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command("ping"), compound = "left", font = ("Arial", 18), bd=0, relief = "flat", cursor = "heart", bg="yellow")
 ping_btn.pack()
 
-tracert_btn = tk.Button(frame, text="trace route", command=lambda:do_command("tracert"))
+tracert_btn = tk.Button(frame, text="trace route", command=lambda:do_command("tracert"), compound = "left", font = ("Arial", 18), bd=0, relief = "flat", cursor = "cross", bg="red")
 tracert_btn.pack()
 
-ns_btn = tk.Button(frame, text="ns lookup", command=lambda:do_command("nslookup"))
+ns_btn = tk.Button(frame, text="ns lookup", command=lambda:do_command("nslookup"), compound = "left", font = ("Arial", 18), bd=0, relief = "flat", cursor = "star", bg="green")
 ns_btn.pack()
+
+save_btn = tk.Button(frame, text="save", command=lambda:mSave(), compound = "left", font = ("Arial", 18), bd=0, relief = "flat", cursor = "circle", bg="blue")
+save_btn.pack()
 
 #add extra commands
 
